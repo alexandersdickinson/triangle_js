60be46410dab1601afd3d4b38e33729d49249d68
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-11-20 13:21:58 -0800

    Everything works.

--- js/scripts.js
+++ js/scripts.js
@@ -42,3 +42,32 @@ function isScalene(s1, s2, s3){
 		return true;
 	}
 }
+
+$(document).ready(function(){
+	$("#blanks form").submit(function(event){
+		var s1 = parseInt($("input#s1").val(), 10);
+		var s2 = parseInt($("input#s2").val(), 10);
+		var s3 = parseInt($("input#s3").val(), 10);
+		if (!(isTriangle(s1, s2, s3))){
+			$("#not-triangle").show();
+			$("#is-triangle").hide();
+		}
+		else if (isEquilateral(s1, s2, s3)){
+			$("#triangle-type").text("n equilateral");
+			$("#is-triangle").show();
+			$("#not-triangle").hide();
+		}
+		else if (isIsosceles(s1, s2, s3)){
+			$("#triangle-type").text("n isosceles");
+			$("#is-triangle").show();
+			$("#not-triangle").hide();
+		}
+		else{
+			$("#triangle-type").text("scalene");
+			$("#is-triangle").show();
+			$("#not-triangle").hide();
+		}
+		
+		event.preventDefault();
+	});
+});
